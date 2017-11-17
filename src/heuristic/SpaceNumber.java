6d5c429b11ad1446bfc3afb4fbf702f4fb55532e
cathybgyz <lvzimeng@hotmail.com>
2017-11-16 23:30:24 -0500

    init SpaceNumber

---
+++ src/heuristic/SpaceNumber.java
@@ -0,0 +1,17 @@
+package heuristic;
+
+import game2048.Tile;
+
+/**
+ * Heuristic function to calculate the space number and give a score
+ * 16 space = 16, 0 space = 0 
+ * Author: Zimeng Lyu
+ * **/
+public class SpaceNumber implements Heuristic {
+	@Override
+	public double function(Tile[] tile) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
