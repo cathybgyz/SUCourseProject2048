e35dbf804545d8a26a76cfbeb01fea362aeb033e
xanderzeng <30305311+xanderzeng@users.noreply.github.com>
2017-11-28 22:08:30 -0500

    Adjust SquareArea
    Problem debugged

--- src/heuristic/SquareArea.java
+++ src/heuristic/SquareArea.java
@@ -13,27 +13,20 @@ public class SquareArea implements Heuristic{
 	@Override
 	public double function(Tile[] tile) {
 		int n = tile.length;
-		int l=0;
-		int w=0;
-		int lcount=0;
-		int[] wcount = new int[4];
-		for(int i:wcount)i=0;
+		int lStart=4;
+		int lEnd = -1;
+		int wStart = 4;
+		int wEnd = -1;
 		
 		for(int i=0;i<n;i++) {
-			if(tile[i].getValue() != 0) {
-				lcount++;
-				wcount[i%4]++;
-			}
-			if(i%4 == 3) {
-				l = Math.max(lcount, l);
-				lcount = 0;
-			}
+			if(tile[i].getCol()<lStart&&tile[i].getValue()!=0) lStart = tile[i].getCol();
+			if(tile[i].getCol()>lEnd&&tile[i].getValue()!=0) lEnd = tile[i].getCol();
+			if(tile[i].getRow()<wStart&&tile[i].getValue()!=0) wStart = tile[i].getRow();
+			if(tile[i].getRow()>wEnd&&tile[i].getValue()!=0) wEnd = tile[i].getRow();
 		}
-		w = wcount[0];
-		for(int i:wcount) {
-			if(w>i) w = i;
-		}
+		int w = wEnd-wStart + 1;
+		int l = lEnd - lStart + 1;
 		int area = w * l;
-		return (double)(16-area);
+		return (double)area;
 	}
 }
