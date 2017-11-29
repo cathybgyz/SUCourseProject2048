d3d0573f3e0f2249e66e37aaea105b70f1c4681f
Qinzhe Zhang <qinzhez@gmail.com>
2017-11-28 21:18:47 -0500

    update for test

--- src/test.java
+++ src/test.java
@@ -11,10 +11,13 @@ public class test {
 		int testTime = 100;
 		int[] element = {2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,8,8,8,16,16,16,32,32,32,64,64,128,128,256,512,1024,2048};
 		Random rand = new Random();
+		
+		// Change the function object below
 		Heuristic func = new SameNumberDistance();
 		double score = 0;
 
 		for (int cur = 0; cur < testTime; cur++) {
+			System.out.println("Case "+cur);
 			for (int i = 0; i < 4; i++) {
 				for (int j = 0; j < 4; j++) {
 					input[i*4+j] = new Tile(i,j,element[(rand.nextInt(1000)*11+23)%element.length]);
