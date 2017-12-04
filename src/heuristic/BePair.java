9f2c921e62acf1ed3a080161623ef5b8446265c5
Qinzhe Zhang <qinzhez@gmail.com>
2017-12-03 20:50:03 -0500

    go 2048

--- src/heuristic/BePair.java
+++ src/heuristic/BePair.java
@@ -7,32 +7,48 @@ public class BePair implements Heuristic {
 	@Override
 	public double function(Tile[] board) {
 		int table[] = new int[15];
-		
-		for(int i=0;i<15;i++) {
+
+		for (int i = 0; i < 15; i++) {
 			table[i] = 0;
 		}
-		
-		for (int x=0;x<4;x++){
-			for(int y=0;y<4;y++) {
-				board[x*4+y].setCol(x);
-				board[x*4+y].setRow(y);
+
+		for (int x = 0; x < 4; x++) {
+			for (int y = 0; y < 4; y++) {
+				board[x * 4 + y].setCol(x);
+				board[x * 4 + y].setRow(y);
 			}
 		}
-		
-		for(int x=0;x<3;x++) {
-			for(int y=0;y<3;y++) {
-				if(board[x*4+y].getValue().intValue() == board[(x+1)*4+y].getValue().intValue())
-					table[LogNumber.LogReturn(board[x*4+y].getValue())]++;
-				else if(board[x*4+y].getValue().intValue() == board[(x)*4+y+1].getValue().intValue())
-					table[LogNumber.LogReturn(board[x*4+y].getValue())]++;
+
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				for (int i = 0; i < 4 - x; i++) {
+					if (board[(x + i) * 4 + y].getValue().intValue() == 0)
+						continue;
+					else { 
+						if (board[x * 4 + y].getValue().intValue() == board[(x + i) * 4 + y].getValue().intValue())
+							table[LogNumber.LogReturn(board[x * 4 + y].getValue())]++;
+						break;
+					}
+				}
+				for (int i = 0; i < 4 - x; i++) {
+					if (board[(x) * 4 + y + i].getValue().intValue() == 0)
+						continue;
+					else { 
+						if (board[x * 4 + y].getValue().intValue() == board[(x) * 4 + y + i].getValue().intValue())
+							table[LogNumber.LogReturn(board[x * 4 + y].getValue())]++;
+						break;
+					}
+				}
+				
+					
 			}
 		}
-		
+
 		double score = 0;
-		for(int i=0;i<15;i++) {
-			score += table[i]*i;
+		for (int i = 0; i < 15; i++) {
+			score += table[i] * i;
 		}
-		
+
 		return score;
 	}
 
